f454ba48abbfbc1e411fa59ccda78d50d6ac9c22
Alexandr Gyach <as.gyach@gmail.com>
2024-02-06 16:08:51 +0300

    tweak - remove hard

--- Assets/Source/SceneControl/ViewForButtons/ShopSceneView.cs
+++ Assets/Source/SceneControl/ViewForButtons/ShopSceneView.cs
@@ -8,13 +8,14 @@ using UnityEngine.UI;
 public class ShopSceneView : BaseInitializable
 {
     [SerializeField] private Button _exit;
-    [SerializeField] private List<Button> _buyButtons;
+    [SerializeField] private Button _openWarriors;
+    [SerializeField] private Button _openArmor;
+    [SerializeField] private Button _buyArmor;
+    [SerializeField] private Button _buyWarriors;
     [SerializeField] private Button _closeWindowForBuy;
     [SerializeField] private GameObject _windowForBuy;
     [SerializeField] private GameObject _forWarriors;
     [SerializeField] private GameObject _forArmor;
-    [SerializeField] private Button _buyArmor;
-    [SerializeField] private Button _buyWarriors;
     [SerializeField] private TextMeshProUGUI _ScoreText;
     [SerializeField] private TextMeshProUGUI _DefenseEffect;
     [SerializeField] private TextMeshProUGUI _WarriorsCount;
@@ -27,24 +28,8 @@ public class ShopSceneView : BaseInitializable
         _buyArmor.onClick.AddListener(BuyArmor);
         _buyWarriors.onClick.AddListener(BuyWarriors);
         
-        foreach (var buyButton in _buyButtons)
-        {
-            var buttonName = buyButton.name; // TODO: Верно ли?
-            buyButton.onClick.AddListener(() => Buy(buttonName)); // TODO: Верно ли?
-        }
-        
-        UpdateScoreCycle();
-    }
-
-    private async void UpdateScoreCycle() // TODO: Отказаться от тасков
-    {
-        while (Application.isPlaying)
-        {
-            _ScoreText.text = $"Score: {PlayerVariables.Score}";
-            _DefenseEffect.text = $"Now: {PlayerVariables.DefensePercent * 100f}%";
-            _WarriorsCount.text = $"Now: {PlayerVariables.Warriors}";
-            await Task.Delay(1000);
-        }
+        _openArmor.onClick.AddListener(OpenArmor);
+        _openWarriors.onClick.AddListener(OpenWarriors);
     }
 
     private void BuyArmor() // TODO: Убрать константы
@@ -53,6 +38,8 @@ public class ShopSceneView : BaseInitializable
         {
             PlayerVariables.DefensePercent += 0.05f;
             PlayerVariables.Score -= 500;
+            _ScoreText.text = $"Score: {PlayerVariables.Score}";
+            _DefenseEffect.text = $"Now: {PlayerVariables.DefensePercent * 100f}%";
         }
     }
 
@@ -62,6 +49,8 @@ public class ShopSceneView : BaseInitializable
         {
             PlayerVariables.Warriors++;
             PlayerVariables.Score -= 10;
+            _ScoreText.text = $"Score: {PlayerVariables.Score}";
+            _WarriorsCount.text = $"Now: {PlayerVariables.Warriors}";
         }
     }
 
@@ -70,20 +59,16 @@ public class ShopSceneView : BaseInitializable
         SceneSwitcher.Switch("Main");
     }
     
-    private void Buy(string buttonName) // TODO: Верно ли?
+    private void OpenWarriors()
     {
         _windowForBuy.SetActive(true);
-        switch (buttonName)
-        {
-            case "BuyArmor":
-                _forArmor.SetActive(true);
-                break;
-            case "BuyWarriors":
-                _forWarriors.SetActive(true);
-                break;
-            default:
-                throw new Exception($"Shop is not included {buttonName}");
-        }
+        _forWarriors.SetActive(true);
+    }
+    
+    private void OpenArmor()
+    {
+        _windowForBuy.SetActive(true);
+        _forArmor.SetActive(true);
     }
 
     private void CloseWindowForBuy()
