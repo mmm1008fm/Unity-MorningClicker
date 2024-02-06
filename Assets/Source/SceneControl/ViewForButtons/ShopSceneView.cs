e56550b5cfdecef3f3ae22a8255c1cd672ef2e99
Alexandr Gyach <as.gyach@gmail.com>
2024-02-06 16:48:54 +0300

    tweak - added buy-button component

--- Assets/Source/SceneControl/ViewForButtons/ShopSceneView.cs
+++ Assets/Source/SceneControl/ViewForButtons/ShopSceneView.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -16,42 +13,14 @@ public class ShopSceneView : BaseInitializable
     [SerializeField] private GameObject _windowForBuy;
     [SerializeField] private GameObject _forWarriors;
     [SerializeField] private GameObject _forArmor;
-    [SerializeField] private TextMeshProUGUI _ScoreText;
-    [SerializeField] private TextMeshProUGUI _DefenseEffect;
-    [SerializeField] private TextMeshProUGUI _WarriorsCount;
 
     public override void Initialize()
     {
         _exit.onClick.AddListener(Exit);
         _closeWindowForBuy.onClick.AddListener(CloseWindowForBuy);
         
-        _buyArmor.onClick.AddListener(BuyArmor);
-        _buyWarriors.onClick.AddListener(BuyWarriors);
-        
         _openArmor.onClick.AddListener(OpenArmor);
         _openWarriors.onClick.AddListener(OpenWarriors);
-    }
-
-    private void BuyArmor() // TODO: Убрать константы
-    {
-        if (PlayerVariables.Score >= 500)
-        {
-            PlayerVariables.DefensePercent += 0.05f;
-            PlayerVariables.Score -= 500;
-            _ScoreText.text = $"Score: {PlayerVariables.Score}";
-            _DefenseEffect.text = $"Now: {PlayerVariables.DefensePercent * 100f}%";
-        }
-    }
-
-    private void BuyWarriors() // TODO: Убрать константы
-    {
-        if (PlayerVariables.Score >= 10)
-        {
-            PlayerVariables.Warriors++;
-            PlayerVariables.Score -= 10;
-            _ScoreText.text = $"Score: {PlayerVariables.Score}";
-            _WarriorsCount.text = $"Now: {PlayerVariables.Warriors}";
-        }
     }
 
     private void Exit()
