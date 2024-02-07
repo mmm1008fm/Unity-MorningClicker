abdf91fe8af203870fe03e2aa803481965bbcee5
Alexandr Gyach <as.gyach@gmail.com>
2024-02-07 17:42:36 +0300

    feat - added extensibility

--- Assets/Source/SceneControl/ViewForButtons/ShopSceneView.cs
+++ Assets/Source/SceneControl/ViewForButtons/ShopSceneView.cs
@@ -1,53 +1,47 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+[System.Serializable]
+public class ButtonViewPair
+{
+    [field: SerializeField] public Button Button { get; private set; }
+    [field: SerializeField] public GameObject GameObject { get; private set; }
+    
+    
+}
+
 public class ShopSceneView : BaseInitializable
 {
+    [SerializeField] private List<ButtonViewPair> _buttonViewPairs;
+    [SerializeField] private List<GameObject> _objectsToClose;
+    
     [SerializeField] private Button _exit;
     [SerializeField] private Button _closeWindowForBuy;
-    [SerializeField] private Button _openWarriors;
-    [SerializeField] private Button _openArmor;
-    [SerializeField] private Button _openWindmill;
     [SerializeField] private GameObject _windowForBuy;
-    [SerializeField] private GameObject _forWarriors;
-    [SerializeField] private GameObject _forArmor;
-    [SerializeField] private GameObject _forWindmill;
 
     public override void Initialize()
     {
         _exit.onClick.AddListener(Exit);
         _closeWindowForBuy.onClick.AddListener(CloseWindowForBuy);
         
-        _openArmor.onClick.AddListener(OpenArmor);
-        _openWarriors.onClick.AddListener(OpenWarriors);
-        _openWindmill.onClick.AddListener(OpenWindmill);
+        foreach (var buttonViewPair in _buttonViewPairs)
+        {
+            buttonViewPair.Button.onClick.AddListener(() =>
+            {
+                _windowForBuy.SetActive(true);
+                buttonViewPair.GameObject.SetActive(true);
+            });
+        }
     }
 
     private void Exit() => SceneSwitcher.Switch("Main");
-    
-    private void OpenWarriors()
-    {
-        _windowForBuy.SetActive(true);
-        _forWarriors.SetActive(true);
-    }
-    
-    private void OpenArmor()
-    {
-        _windowForBuy.SetActive(true);
-        _forArmor.SetActive(true);
-    }
-
-    private void OpenWindmill()
-    {
-        _windowForBuy.SetActive(true);
-        _forWindmill.SetActive(true);
-    }
 
     private void CloseWindowForBuy()
     {
-        _windowForBuy.SetActive(false);
-        _forArmor.SetActive(false);
-        _forWarriors.SetActive(false);
-        _forWindmill.SetActive(false);
+        foreach (var o in _objectsToClose)
+        {
+            o.SetActive(false);
+        }
     }
 }
