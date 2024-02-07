02efe2970ef259e3ea6840332a9dbfa80bf73f97
Alexandr Gyach <as.gyach@gmail.com>
2024-02-08 00:16:28 +0300

    fix - added layers in script

--- Assets/Source/CoinParticleController.cs
+++ Assets/Source/CoinParticleController.cs
@@ -1,16 +1,42 @@
+using System;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class CoinParticleController : MonoBehaviour
 {
     [SerializeField] private ParticleSystem _particleSystemPrefab;
     [SerializeField] private List<AudioSource> _sounds;
+    [SerializeField] private UIlayer _layer;
+    private float _posZ;
 
     public void SpawnParticle()
     {
+        switch (_layer)
+        {
+            case UIlayer.Front:
+                _posZ = -50f;
+                break;
+            case UIlayer.Back:
+                _posZ = 50f;
+                break;
+            case UIlayer.Center:
+                _posZ = 0f;
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
         _sounds[Random.Range(0, _sounds.Count)].Play();
-        ParticleSystem particleSystemInstance = Instantiate(_particleSystemPrefab, transform);
+        var particleSystemInstance = Instantiate(_particleSystemPrefab, new Vector3(0f, 0f, _posZ), quaternion.identity);
         particleSystemInstance.Play();
-        Destroy(particleSystemInstance.gameObject, particleSystemInstance.main.duration + 10f);
+        Destroy(particleSystemInstance.gameObject, particleSystemInstance.main.duration);
+    }
+    
+    private enum UIlayer
+    {
+        Front,
+        Back,
+        Center
     }
 }
