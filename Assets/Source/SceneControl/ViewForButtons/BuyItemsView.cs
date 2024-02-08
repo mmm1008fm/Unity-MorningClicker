6435639d8bca1c79033c66a9789bc6ab8ae3aeba
Alexandr Gyach <as.gyach@gmail.com>
2024-02-08 17:53:51 +0300

    fix - save

--- Assets/Source/SceneControl/ViewForButtons/BuyItemsView.cs
+++ Assets/Source/SceneControl/ViewForButtons/BuyItemsView.cs
@@ -9,9 +9,10 @@ public class BuyItemsView : MonoBehaviour
 	[SerializeField] private TextMeshProUGUI _costText;
 	[SerializeField] private Button _buyButton;
 	[SerializeField] private ShopItem _item;
-	[SerializeField] private int _startCost;
-	[SerializeField] private float _costMultiplier;
-	[SerializeField] private float _count;
+	[SerializeField] private int _startCost = 10;
+	[SerializeField] private float _costMultiplier = 2f;
+	[SerializeField] private float _count = 1f;
+	[SerializeField] private int _costDefaultMin;
 
 	public void Awake()
 	{
@@ -31,21 +32,30 @@ public class BuyItemsView : MonoBehaviour
 			case ShopItem.Armor:
 				_effect.text = $"Defense: {Math.Round(PlayerVariables.DefensePercent * 100f)}%";
 				_costText.text = $"Buy armor {_startCost}$";
+                _startCost = PlayerPrefs.GetInt("ArmorCost", _startCost);
 				break;
 			case ShopItem.Warrior:
 				_effect.text = $"Warriors count: {PlayerVariables.Warriors}";
 				_costText.text = $"Buy warrior {_startCost}$";
+                _startCost = PlayerPrefs.GetInt("WarriorsCost", _startCost);
 				break;
 			case ShopItem.Windmill:
 				_effect.text = $"Windmill power: {PlayerVariables.WindmillPower}";
 				_costText.text = $"Buy windmill {_startCost}$";
+                _startCost = PlayerPrefs.GetInt("WindmillCost", _startCost);
 				break;
 			case ShopItem.PerClick:
 				_effect.text = $"Per click: {PlayerVariables.PerClick}";
 				_costText.text = $"Buy click {_startCost}$";
+                _startCost = PlayerPrefs.GetInt("ClickCost", _startCost);
 				break;
 			default:
 				throw new ArgumentOutOfRangeException();
+		}
+
+		if (_startCost == 0)
+		{
+			_startCost = _costDefaultMin;
 		}
 	}
 
@@ -63,15 +73,19 @@ public class BuyItemsView : MonoBehaviour
 		{
 			case ShopItem.Armor:
 				PlayerVariables.DefensePercent += _count;
+				PlayerPrefs.SetInt("ArmorCost", _startCost);
 				break;
 			case ShopItem.Warrior:
 				PlayerVariables.Warriors += (int)_count;
+				PlayerPrefs.SetInt("WarriorCost", _startCost);
 				break;
 			case ShopItem.Windmill:
 				PlayerVariables.WindmillPower += (int)_count;
+				PlayerPrefs.SetInt("WindmillCost", _startCost);
 				break;
 			case ShopItem.PerClick:
 				PlayerVariables.PerClick += (int)_count;
+				PlayerPrefs.SetInt("ClickCost", _startCost);
 				break;
 			default:
 				throw new ArgumentOutOfRangeException();
